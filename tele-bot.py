ac2ab5880e1cd066ac32dbea8504e94bc6ea8855
almosh443 <mr.almosh443@mail.ru>
2021-04-02 00:08:25 +0300

    hotfix

--- tele-bot.py
+++ tele-bot.py
@@ -1026,7 +1026,7 @@ schedule.every(1).hours.do(backup)
 def bot3():
     os.system('python tele-bot3.py')
 
-threading.Thread(target=bot3).start()#start new bot
+#TODO threading.Thread(target=bot3).start()#start new bot
 
 def sp():
     while True:
